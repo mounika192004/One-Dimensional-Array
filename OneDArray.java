434edbe4a813c15a08758ad25e4f80babfe0d32d
Mounika <nagargojemounika@gmail.com>
2025-01-29 05:25:23 +0530

    Add files via upload

---
+++ OneDArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class OneDArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Array elements:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        scanner.close();
+    }
+}
+
+
